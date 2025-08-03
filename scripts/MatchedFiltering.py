96360dfd291f2745b74e128d05322e6f5d4ffbc0
Ben Holmes <149116719+MrBenjaminHolmes@users.noreply.github.com>
2025-08-03 23:55:00 +0100

    Update MatchedFiltering.py

--- scripts/MatchedFiltering.py
+++ scripts/MatchedFiltering.py
@@ -2,7 +2,15 @@
 sample_rate = 1024
 data_length = 1024
 
-#Injection Time
+# Generate Signal
+hp1, _ = get_td_waveform(approximant="IMRPhenomD",
+                         mass1=10,
+                         mass2=10,
+                         delta_t=1.0/sample_rate,
+                         f_lower=25)
+hp1 = hp1 / max(numpy.correlate(hp1, hp1, mode='full'))**0.5
+
+# Injection Time
 fixed_time = 500
 waveform_start = int(fixed_time * sample_rate)
 
@@ -16,10 +24,10 @@ psd = pycbc.psd.aLIGOZeroDetHighPower(flen, delta_f, flow)
 delta_t = 1.0 / sample_rate
 ts = pycbc.noise.noise_from_psd(data_length*sample_rate, delta_t, psd, seed=127)
 
-#Inject FAKE event into code
+# Inject FAKE event into code
 ts[waveform_start:waveform_start+len(hp1)] += hp1.numpy() * 1E-20
 
-#PSD's for Whittening
+# PSD's for Whittening
 flow = 10.0
 delta_f = 1.0 / data_length
 flen = int(sample_rate / (2 * delta_f)) + 1
@@ -35,16 +43,16 @@ psd_td[len(psd_td) - 1] = psd_td[len(psd_td) - 2]
 psd_hp1[0] = psd_hp1[1]
 psd_hp1[len(psd_hp1) - 1] = psd_hp1[len(psd_hp1) - 2]
 
-#Whitten the Noise and the Waveform
+# Whitten the Noise and the Waveform
 data_whitened = (ts.to_frequencyseries() / psd_td**0.5).to_timeseries()
 hp1_whitened = (hp1.to_frequencyseries() / psd_hp1**0.5).to_timeseries() * 1E-21
 
-#Create NumPy Arrays for Data
+# Create NumPy Arrays for Data
 cross_correlation = numpy.zeros([len(data)-len(hp1)])
 hp1n = hp1_whitened.numpy()
 datan = data_whitened.numpy()
 
-#Slide Template Across Sample
+# Slide Template Across Sample
 for i in range(len(datan) - len(hp1n)):
     cross_correlation[i] = (hp1n * datan[i:i+len(hp1n)]).sum() # Calulcate Product
 
