6fe51757a85f826aa5b13d8e468726b17cd5ee29
Ben Holmes <149116719+MrBenjaminHolmes@users.noreply.github.com>
2025-08-03 23:40:20 +0100

    Create MatchedFiltering.py

---
+++ scripts/MatchedFiltering.py
@@ -0,0 +1,60 @@
+# Define Sample Properties
+sample_rate = 1024
+data_length = 1024
+
+#Injection Time
+fixed_time = 500
+waveform_start = int(fixed_time * sample_rate)
+
+# Create sample PSD
+flow = 10.0
+delta_f = 1.0 / 128
+flen = int(sample_rate / (2 * delta_f)) + 1
+psd = pycbc.psd.aLIGOZeroDetHighPower(flen, delta_f, flow)
+
+# Generate Fake Noise from PSD
+delta_t = 1.0 / sample_rate
+ts = pycbc.noise.noise_from_psd(data_length*sample_rate, delta_t, psd, seed=127)
+
+#Inject FAKE event into code
+ts[waveform_start:waveform_start+len(hp1)] += hp1.numpy() * 1E-20
+
+#PSD's for Whittening
+flow = 10.0
+delta_f = 1.0 / data_length
+flen = int(sample_rate / (2 * delta_f)) + 1
+psd_td = pycbc.psd.aLIGOZeroDetHighPower(flen, delta_f, 0)
+
+delta_f = sample_rate / float(len(hp1))
+flen = int(sample_rate / (2 * delta_f)) + 1
+psd_hp1 = pycbc.psd.aLIGOZeroDetHighPower(flen, delta_f, 0)
+
+# Avoding NaNs and 0 Division
+psd_td[0] = psd_td[1]
+psd_td[len(psd_td) - 1] = psd_td[len(psd_td) - 2]
+psd_hp1[0] = psd_hp1[1]
+psd_hp1[len(psd_hp1) - 1] = psd_hp1[len(psd_hp1) - 2]
+
+#Whitten the Noise and the Waveform
+data_whitened = (ts.to_frequencyseries() / psd_td**0.5).to_timeseries()
+hp1_whitened = (hp1.to_frequencyseries() / psd_hp1**0.5).to_timeseries() * 1E-21
+
+#Create NumPy Arrays for Data
+cross_correlation = numpy.zeros([len(data)-len(hp1)])
+hp1n = hp1_whitened.numpy()
+datan = data_whitened.numpy()
+
+#Slide Template Across Sample
+for i in range(len(datan) - len(hp1n)):
+    cross_correlation[i] = (hp1n * datan[i:i+len(hp1n)]).sum() # Calulcate Product
+
+
+# Plotting
+plt.figure()
+times = numpy.arange(len(datan) - len(hp1n)) / float(sample_rate)
+plt.plot(times, cross_correlation)
+plt.plot([waveform_start/float(sample_rate), waveform_start/float(sample_rate)],
+           [(min(cross_correlation))*1.1,(max(cross_correlation))*1.1],'r:')
+plt.xlabel('Time (s)')
+plt.ylabel('Cross-correlation')
+plt.show()
